9e4c5465efc49dbee81aad7011d285fc10771692
Rahul Kesharawni <rahulk@qlogic.io>
2018-12-20 19:45:27 +0530

    removed timeunit

--- bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/config/TestBigtableVaneerSettingsFactory.java
+++ bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/config/TestBigtableVaneerSettingsFactory.java
@@ -121,7 +121,6 @@ public class TestBigtableVaneerSettingsFactory {
   @Test
   public void testWithActualTables() throws Exception{
     Assume.assumeFalse(endToEndTest);
-    TimeUnit.SECONDS.sleep(10);
     if (adminClient == null || dataClient == null) {
       initializeClients();
     }
