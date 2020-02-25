d83b309c489e7942d036c3b51d25610aec4b7280
Rahul Kesharwani <rahulk@qlogic.io>
2020-02-25 13:59:57 +0530

    docs: add javadoc for BigtableCoreConstants

--- bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/config/BigtableCoreConstants.java
+++ bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/config/BigtableCoreConstants.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+/** Default values for various bigtable operation configuration. */
 @InternalApi("For internal usage only")
 public class BigtableCoreConstants {
 
